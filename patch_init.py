d051805b5fb5118718c454fd72ef993a035f383c
nageshfreebird-cloud <nageshfreebird@gmail.com>
2026-09-14 20:08:16 +0530

    Add files via upload

---
+++ patch_init.py
@@ -0,0 +1,15 @@
+with open("manager-attendance.html", "r", encoding="utf-8") as f:
+    html = f.read()
+
+import re
+
+# Find:
+#      allAttendance = await getTeamAttendance();
+#      renderTable();
+#    } catch(err) {
+
+html = re.sub(r'allAttendance = await getTeamAttendance\(\);\s*renderTable\(\);', 'allAttendance = await getTeamAttendance();\n    renderTable();\n    if (window.loadPendingLeaves) await window.loadPendingLeaves();', html)
+
+with open("manager-attendance.html", "w", encoding="utf-8") as f:
+    f.write(html)
+print("Injected")
